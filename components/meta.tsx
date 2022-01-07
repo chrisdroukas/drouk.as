2abca5e776d55dc591f44ca544483c5664672e6f
Chris Droukas <c.droukas@gmail.com>
2022-01-07 16:03:07 -0500

    rm msft

--- components/meta.tsx
+++ components/meta.tsx
@@ -31,9 +31,6 @@ const Meta = () => {
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
