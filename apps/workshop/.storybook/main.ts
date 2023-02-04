fc241e830cb30e6b1bd51b56481485bc895f78ab
Chris Droukas <c.droukas@gmail.com>
2023-02-04 15:08:56 -0500

    storybook uses typescript main, more accurate types, vite merger

---
+++ apps/workshop/.storybook/main.ts
@@ -0,0 +1,37 @@
+import { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
+  addons: [
+    "storybook-addon-swc",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+  framework: { name: "@storybook/react-vite", options: {} },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      /**
+       * @todo
+       * Temporary workaround for an error `process is not defined`.
+       * @see
+       * https://github.com/storybookjs/storybook/issues/18920
+       */
+      define: { "process.env": {} },
+      resolve: {
+        alias: [
+          {
+            find: "ui",
+            replacement: path.resolve(__dirname, "../../../packages/ui/"),
+          },
+        ],
+      },
+    });
+  },
+};
+
+export default config;
