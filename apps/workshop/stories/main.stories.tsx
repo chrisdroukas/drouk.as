99ff950296079897ee8839f5da123ecaa01d5154
Chris Droukas <c.droukas@gmail.com>
2023-02-04 15:09:50 -0500

    adds sample story

---
+++ apps/workshop/stories/main.stories.tsx
@@ -0,0 +1,11 @@
+import { HeroTitle } from "ui";
+
+export default { title: "Logo" };
+
+export function Droukas() {
+  return (
+    <div style={{ padding: 40 }}>
+      <HeroTitle />
+    </div>
+  );
+}
