7bf64b22def0477efe921b16adcb820ddbbb4258
Chris Droukas <c.droukas@gmail.com>
2023-02-05 14:20:07 -0500

    enhanced stringsfile

--- packages/ui/strings/index.ts
+++ packages/ui/strings/index.ts
@@ -1,4 +1,11 @@
-export const strings = {
+export const Strings = {
+  hero: {
+    title: "Chris Droukas is a design technologist in New York.",
+    body: "Designing at Goldman Sachs for Goldman Sachs Developer and Marquee. Previously: Priceline, SocialCode, and Washington Post Labs.",
+  },
+  errorBoundary: {
+    didCatch: "Uncaught Error:",
+  },
   debug: {
     failure:
       "An instance of the debugger is running in production and must be removed before deploying this application.",
