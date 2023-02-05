dbdc890dfe8b2f0b3ab3cb1a21dcadf180ba203b
Chris Droukas <c.droukas@gmail.com>
2023-02-05 14:22:17 -0500

    base component prop docs

--- packages/ui/base-component/index.tsx
+++ packages/ui/base-component/index.tsx
@@ -1,3 +1,10 @@
 export interface BaseComponentProps {
+  /**
+   * Optionally enables logging lifecycle events
+   * and debug messages to the console.
+   *
+   * This value must be disabled for production
+   * builds.
+   */
   debug?: boolean;
 }
