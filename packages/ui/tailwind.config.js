99dd1f6eebdde2baed5c9c3184dc40f3e34c3035
Chris Droukas <c.droukas@gmail.com>
2023-06-23 09:37:03 -0400

    upgrade shadcnui

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,13 +2,13 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./ui/**/*.{ts,tsx}",
-    "../../packages/ui/components/**/*.{ts,tsx}",
-  ],
   darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -54,8 +54,8 @@ module.exports = {
         },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
