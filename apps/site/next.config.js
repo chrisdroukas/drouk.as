10a12703bbad62650f7dd3b760b6db8cf528a31c
Chris Droukas <c.droukas@gmail.com>
2023-06-25 17:00:15 -0400

    testing again

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -7,13 +5,12 @@ const config = {
   transpilePackages: ["ui"],
   experimental: {
     appDir: true,
-    outputFileTracingRoot: path.join(__dirname, "../../"),
+    // outputFileTracingRoot: path.join(__dirname, "../../"),
     outputFileTracingExcludes: {
       "*": [
-        // prettier-ignore
-        'node_modules/@swc/core-linux-x64-gnu',
-        "node_modules/@swc/core-linux-x64-musl",
-        "node_modules/@esbuild/linux-x64",
+        "./**/node_modules/@swc/core-linux-x64-gnu",
+        "./**/node_modules/@swc/core-linux-x64-musl",
+        "./**/node_modules/esbuild/linux",
       ],
     },
   },
