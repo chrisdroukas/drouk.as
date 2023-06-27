6f4aa875c45c29dcdf6a8ae8ce51aff031659a1b
Chris Droukas <c.droukas@gmail.com>
2023-06-27 18:52:27 -0400

    content updates

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
@@ -7,16 +5,14 @@ const config = {
   transpilePackages: ["ui"],
   experimental: {
     appDir: true,
-    outputFileTracingIgnores: ["**swc+core**", "**esbuild**"],
-    // outputFileTracingRoot: path.join(__dirname, "../../"),
-    // outputFileTracingExcludes: {
-    //   "*": [
-    //     // prettier-ignore
-    //     'node_modules/@swc/core-linux-x64-gnu',
-    //     "node_modules/.pnpm/@swc+core-linux-x64-musl@1.3.62",
-    //     "node_modules/@esbuild/linux-x64",
-    //   ],
-    // },
+    /**
+     * SWC and ESBuild are being included in output incorrectly
+     * due to some odd bundling behavior from Storybook in
+     * another package.
+     * @todo
+     * @see https://github.com/vercel/next.js/issues/42641
+     */
+    outputFileTracingExcludes: ["**swc+core**", "**esbuild**"],
   },
 };
 
