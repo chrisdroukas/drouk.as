7f6535be695f53fa738e2a6974a63471aeebeb5c
Chris Droukas <c.droukas@gmail.com>
2023-08-11 17:39:54 -0400

    temporarily remove some items from navigation

--- apps/site/configuration/navigation.ts
+++ apps/site/configuration/navigation.ts
@@ -7,21 +7,21 @@ export const navigationConfiguration: NavigationConfiguration = {
       title: Strings.configuration.navigation.about.title,
       href: Strings.configuration.navigation.about.href,
     },
-    {
-      title: Strings.configuration.navigation.work.title,
-      href: Strings.configuration.navigation.work.href,
-    },
-    {
-      title: Strings.configuration.navigation.writing.title,
-      href: Strings.configuration.navigation.writing.href,
-    },
-    {
-      title: Strings.configuration.navigation.running.title,
-      href: Strings.configuration.navigation.running.href,
-    },
-    {
-      title: Strings.configuration.navigation.labs.title,
-      href: Strings.configuration.navigation.labs.href,
-    },
+    // {
+    //   title: Strings.configuration.navigation.work.title,
+    //   href: Strings.configuration.navigation.work.href,
+    // },
+    // {
+    //   title: Strings.configuration.navigation.writing.title,
+    //   href: Strings.configuration.navigation.writing.href,
+    // },
+    // {
+    //   title: Strings.configuration.navigation.running.title,
+    //   href: Strings.configuration.navigation.running.href,
+    // },
+    // {
+    //   title: Strings.configuration.navigation.labs.title,
+    //   href: Strings.configuration.navigation.labs.href,
+    // },
   ],
 };
