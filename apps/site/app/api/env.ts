580f557e1f6f30555994f7132449019cfc421f32
Chris Droukas <c.droukas@gmail.com>
2023-08-17 15:37:57 -0400

    update

--- apps/site/app/api/env.ts
+++ apps/site/app/api/env.ts
@@ -1,8 +1,3 @@
 export const apiEnvironment = () => {
-  switch (process.env.VERCEL_ENV) {
-    case "development":
-      return `http://${process.env.NEXT_PUBLIC_BASE_URL}/api`;
-    default:
-      return `https://${process.env.NEXT_PUBLIC_BASE_URL}/api`;
-  }
+  return process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
 };
