897fd194f22f5b7475d883c19e90fa87c92c9a25
Chris Droukas <c.droukas@gmail.com>
2023-08-17 14:14:40 -0400

    use globals

--- packages/jest-presets/jest/node/jest-setup.ts
+++ packages/jest-presets/jest/node/jest-setup.ts
@@ -1,16 +1,21 @@
 import "@testing-library/jest-dom";
+import { jest, beforeAll, afterAll } from "@jest/globals";
 
 const originalMatchMedia = window.matchMedia;
 
 beforeAll(() => {
-  window.matchMedia = jest.fn().mockImplementation((query) => {
-    return {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
       matches: false,
       media: query,
       onchange: null,
-      addListener: jest.fn(),
-      removeListener: jest.fn(),
-    };
+      addListener: jest.fn() /* Deprecated */,
+      removeListener: jest.fn() /* Deprecated */,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
   });
 });
 
