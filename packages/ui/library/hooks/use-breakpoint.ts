03618ffd37fba45acc497404501d9e1d52d1c8bd
Chris Droukas <c.droukas@gmail.com>
2023-08-25 15:35:56 -0400

    restore import

--- packages/ui/library/hooks/use-breakpoint.ts
+++ packages/ui/library/hooks/use-breakpoint.ts
@@ -2,7 +2,7 @@ import { useMediaQuery } from "react-responsive";
 import resolveConfig from "tailwindcss/resolveConfig";
 import { Config, ScreensConfig } from "tailwindcss/types/config";
 
-import tailwindConfig from "../../tailwind.config.js";
+import * as tailwindConfig from "tailwind.config";
 
 const fullConfig = resolveConfig(tailwindConfig as unknown as Config);
 
