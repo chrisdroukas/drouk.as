e9450996d4b8fa2c50d08d9b4b30017f3a5dfb27
Chris Droukas <c.droukas@gmail.com>
2023-11-09 17:17:52 -0500

    disables console

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["next", "turbo", "prettier", "eslint:recommended"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "no-console": "error",
     "no-unused-vars": "error",
   },
   parserOptions: {
