1d1801287bcced444e705d50dadf66e27daab111
Chris Droukas <c.droukas@gmail.com>
2023-12-07 15:37:10 -0500

    patch breakpoints

--- packages/ui/library/hooks/use-breakpoint.ts
+++ packages/ui/library/hooks/use-breakpoint.ts
@@ -1,12 +1,13 @@
 import { useMediaQuery } from "react-responsive";
 import resolveConfig from "tailwindcss/resolveConfig";
-import { Config, ScreensConfig } from "tailwindcss/types/config";
-
-import * as tailwindConfig from "../../tailwind.config";
+import tailwindConfig from "../../tailwind.config";
+import { Config } from "tailwindcss/types/config";
 
 const fullConfig = resolveConfig(tailwindConfig as unknown as Config);
 
-const breakpoints = fullConfig?.theme?.screens || {
+type BreakpointsType = Record<keyof typeof fullConfig.theme.screens, string>;
+
+const breakpoints: BreakpointsType = fullConfig?.theme?.screens || {
   xs: "480px",
   sm: "640px",
   md: "768px",
@@ -14,25 +15,24 @@ const breakpoints = fullConfig?.theme?.screens || {
   xl: "1280px",
 };
 
-type BreakpointKey = keyof ScreensConfig;
+export function useBreakpoint(breakpointKey: keyof typeof breakpoints) {
+  const breakpointValue = breakpoints[breakpointKey];
 
-export function useBreakpoint<K extends string>(breakpointKey: K) {
-  const breakpointValue = breakpoints[breakpointKey as BreakpointKey];
-
-  const bool = useMediaQuery({
+  const isBelowBreakpoint = useMediaQuery({
     query: `(max-width: ${breakpointValue})`,
   });
 
   const capitalizedKey =
-    breakpointKey?.[0]?.toUpperCase() + breakpointKey?.substring(1);
-
-  type KeyAbove = `isAbove${Capitalize<K>}`;
-  type KeyBelow = `isBelow${Capitalize<K>}`;
+    breakpointKey.charAt(0).toUpperCase() + breakpointKey.slice(1);
 
   return {
-    [breakpointKey]: Number(String(breakpointValue).replace(/[^0-9]/g, "")),
-    [`isAbove${capitalizedKey}`]: !bool,
-    [`isBelow${capitalizedKey}`]: bool,
+    [breakpointKey]: parseInt(breakpointValue, 10),
+    [`isAbove${capitalizedKey}`]: !isBelowBreakpoint,
+    [`isBelow${capitalizedKey}`]: isBelowBreakpoint,
   } as Record<typeof breakpointKey, number> &
-    Record<KeyAbove | KeyBelow, boolean>;
+    Record<
+      | `isAbove${Capitalize<typeof breakpointKey>}`
+      | `isBelow${Capitalize<typeof breakpointKey>}`,
+      boolean
+    >;
 }
