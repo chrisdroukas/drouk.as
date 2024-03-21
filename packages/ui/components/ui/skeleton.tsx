2b60e180de47487b28b78f8d61976680f1e7942e
Chris Droukas <c.droukas@gmail.com>
2024-03-21 11:27:05 -0400

    content updates

--- packages/ui/components/ui/skeleton.tsx
+++ packages/ui/components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/library/utilities"
+import { cn } from "@/library/utilities";
 
 function Skeleton({
   className,
@@ -9,7 +9,7 @@ function Skeleton({
       className={cn("animate-pulse rounded-md bg-primary/10", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
