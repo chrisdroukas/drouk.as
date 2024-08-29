6d1c90c554cb22de2f2af37321adafdb545b2409
Chris Droukas <c.droukas@gmail.com>
2024-08-29 09:12:01 -0400

    patch?

--- apps/site/contentlayer.config.ts
+++ apps/site/contentlayer.config.ts
@@ -106,27 +106,6 @@ export default makeSource({
     rehypePlugins: [
       rehypeSlug,
       [
-        // @todo
-        // @ts-expect-error
-        rehypePrettyCode,
-        {
-          theme: "github-dark",
-          onVisitLine(node: any) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }];
-            }
-          },
-          onVisitHighlightedLine(node: any) {
-            node.properties.className.push("line--highlighted");
-          },
-          onVisitHighlightedWord(node: any) {
-            node.properties.className = ["word--highlighted"];
-          },
-        },
-      ],
-      [
         rehypeAutolinkHeadings,
         {
           properties: {
