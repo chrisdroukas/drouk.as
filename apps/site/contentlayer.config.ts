05ab08eeb94e7e1c6823e453c2dbcac3f33d48bd
Chris Droukas <c.droukas@gmail.com>
2024-08-29 09:21:38 -0400

    cleanup

--- apps/site/contentlayer.config.ts
+++ apps/site/contentlayer.config.ts
@@ -1,5 +1,4 @@
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
