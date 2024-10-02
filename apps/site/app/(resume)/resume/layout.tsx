ea74f4a7b66b4736dbf4dbe3f5519adc66c9fbed
Chris Droukas <c.droukas@gmail.com>
2024-10-02 14:29:49 -0400

    no message

--- apps/site/app/(resume)/resume/layout.tsx
+++ apps/site/app/(resume)/resume/layout.tsx
@@ -1,11 +1,6 @@
 import { ReactNode } from "react";
 import { SiteLayout } from "@/components/layouts";
-import { siteConfiguration } from "#/configuration";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  return (
-    <SiteLayout footer={{ configuration: siteConfiguration }}>
-      {children}
-    </SiteLayout>
-  );
+  return <SiteLayout>{children}</SiteLayout>;
 }
